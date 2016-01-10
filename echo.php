efb0192599d2d45f407da88a4eaa9205db7eabaa
audiofreak9 <audiofreak9@gmail.com>
2016-01-10 17:21:50 -0500

    Update echo.php
    The <a href="https://github.com/bwssytems/ha-bridge">ha-bridge</a> supports dim commands from the Echo. The bridge can pass variables based on your spoken requested percentage. They are ${intensity.percent} (for 1-100) or ${intensity.byte} (for 0-255). By saying, “Alexa, dim the kitchen lights to 50%” that percentage will be passed in the ‘On URL’.

--- echo.php
+++ echo.php
@@ -1,8 +1,15 @@
 <?php
+$hvar = "heyu -c /home/pi/.heyu/x10config ";
 $echo = "Error: No variables!";
 if ((isset($_GET['action'])) && (isset($_GET['hu']))) {
 	$echo = "Echo! ";
-	$command = "heyu -c /home/pi/.heyu/x10config " . $_GET['action'] . " " . $_GET['hu'];
+	if ((isset($_GET['percent'])) && (is_numeric($_GET['percent'])) && ($_GET['percent'] >= 1)) {
+		$level = ceil(22 - (22 / 100 * $_GET['percent']));
+		if ($level == 0) $level = 1; //Prevents an obdim of 0
+			$command = $hvar . "obdim " . $_GET['hu']. " " . $level;
+		}else{
+			$command = $hvar . $_GET['action'] . " " . $_GET['hu'];
+		}
 	exec($command." > /dev/null 2>/dev/null &");
 }
 ?>
@@ -13,6 +20,6 @@ if ((isset($_GET['action'])) && (isset($_GET['hu']))) {
 <title>Echo!</title>
 </head>
 <body>
-  <div><?php echo $echo . $command; ?></div>
+<div><?php echo $echo . $command; ?></div>
 </body>
 </html>
