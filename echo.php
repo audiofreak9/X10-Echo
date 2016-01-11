20c24c6d94d5c8a05b1f5e9f8a11713b9f2742f6
audiofreak9 <audiofreak9@gmail.com>
2016-01-11 14:02:50 -0500

    Update echo.php

--- echo.php
+++ echo.php
@@ -1,14 +1,15 @@
 <?php
 $hvar = "heyu -c /home/pi/.heyu/x10config ";
 $echo = "Error: No variables!";
-if ((isset($_GET['action'])) && (isset($_GET['hu']))) {
+extract($_GET);
+if ((isset($action)) && (isset($hu))) {
 	$echo = "Echo! ";
-	if ((isset($_GET['percent'])) && (is_numeric($_GET['percent'])) && ($_GET['percent'] >= 1)) {
-		$level = ceil(22 - (22 / 100 * $_GET['percent']));
+	if ((isset($percent)) && (is_numeric($percent)) && ($percent >= 1)) {
+		$level = ceil(22 - (22 / 100 * $percent));
 		if ($level == 0) $level = 1; //Prevents an obdim of 0
-		$command = $hvar . "obdim " . $_GET['hu']. " " . $level;
+		$command = $hvar . "obdim " . $hu . " " . $level;
 	}else{
-		$command = $hvar . $_GET['action'] . " " . $_GET['hu'];
+		$command = $hvar . $action . " " . $hu;
 	}
 	exec($command." > /dev/null 2>/dev/null &");
 }
