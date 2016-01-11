9c7c97f00f19301cf724f6d9c4e987c247e26ca2
audiofreak9 <audiofreak9@gmail.com>
2016-01-11 13:25:16 -0500

    Update echo.php

--- echo.php
+++ echo.php
@@ -6,10 +6,10 @@ if ((isset($_GET['action'])) && (isset($_GET['hu']))) {
 	if ((isset($_GET['percent'])) && (is_numeric($_GET['percent'])) && ($_GET['percent'] >= 1)) {
 		$level = ceil(22 - (22 / 100 * $_GET['percent']));
 		if ($level == 0) $level = 1; //Prevents an obdim of 0
-			$command = $hvar . "obdim " . $_GET['hu']. " " . $level;
-		}else{
-			$command = $hvar . $_GET['action'] . " " . $_GET['hu'];
-		}
+		$command = $hvar . "obdim " . $_GET['hu']. " " . $level;
+	}else{
+		$command = $hvar . $_GET['action'] . " " . $_GET['hu'];
+	}
 	exec($command." > /dev/null 2>/dev/null &");
 }
 ?>
